3eb2a4b2bbdd207251674c8b07247030108e5f09
Sam Lim <limsammy1@gmail.com>
2017-03-16 15:07:20 -0600

    Added attr_reader

--- classes_and_instances_playground.rb
+++ classes_and_instances_playground.rb
@@ -1,7 +1,15 @@
 require 'pry'
 
 class Fridge
+  attr_reader :brand, :color, :temperature, :plugged_in, :contents
 
+  def initialize(brand, color, temperature, plugged_in, contents)
+    @brand        = brand
+    @color        = color
+    @temperature  = temperature
+    @plugged_in   = plugged_in
+    @contents     = contents
+  end
 end
 
 binding.pry
