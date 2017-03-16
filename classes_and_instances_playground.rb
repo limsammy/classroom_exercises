d8812a765cf6cf37e50f89afbe6757592e4e27d5
Sam Lim <limsammy1@gmail.com>
2017-03-16 15:48:54 -0600

    End of day commit

--- classes_and_instances_playground.rb
+++ classes_and_instances_playground.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 class Fridge
-  attr_reader :brand, :color, :temperature, :plugged_in, :contents
+  attr_reader :brand, :temperature, :plugged_in, :contents
+
+  attr_accessor :color
 
   def initialize(brand, color, temperature, plugged_in, contents)
     @brand        = brand
@@ -9,6 +11,14 @@ class Fridge
     @temperature  = temperature
     @plugged_in   = plugged_in
     @contents     = contents
+  end
+
+  def temperature_in_celsius
+    (temperature - 32) * 5.0 / 9.0
+  end
+
+  def add_item(new_item)
+    contents << new_item
   end
 end
 
